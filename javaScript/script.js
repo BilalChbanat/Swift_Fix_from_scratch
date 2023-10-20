101d2b1c337b2589bcffb5081627b1ce04b26abe
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-10-20 15:23:33 +0100

    drop down menu on services in header all pages

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,15 @@
 const hamburger_Button = document.querySelector('.hamburger_icon');
 const menu = document.querySelector('.mobile_navigation');
-const header = document.querySelector('header');
-function show_dropdown_menu_() {
+const arrow = document.querySelector('.image_arrow');
+const drop_down_menu_list = document.querySelector('.ul-drop-down-items');
+const drop = document.getElementById('drop_down_menu');
+
+function hamburger() {
     hamburger_Button.classList.toggle('is-active');
     menu.classList.toggle('is-active');
+}
 
+function drop_down() {
+    drop_down_menu_list.classList.toggle('is-active');
+    arrow.classList.toggle('is-active');
 }
