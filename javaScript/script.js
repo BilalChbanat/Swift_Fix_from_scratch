0cfb11d0225c7fd79447b9eb0e180a00a88fbd54
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-10-22 13:02:11 +0100

    adding the onclick on search button display the search input

--- javaScript/script.js
+++ javaScript/script.js
@@ -2,6 +2,8 @@ const hamburger_Button = document.querySelector('.hamburger_icon');
 const menu = document.querySelector('.mobile_navigation');
 const arrow = document.querySelector('.image_arrow');
 const drop_down_menu_list = document.querySelector('.ul-drop-down-items');
+const search_button = document.querySelector('.search-button');
+const input = document.querySelector('.search-bar');
 const drop = document.getElementById('drop_down_menu');
 
 function hamburger() {
@@ -13,3 +15,17 @@ function drop_down() {
     drop_down_menu_list.classList.toggle('is-active');
     arrow.classList.toggle('is-active');
 }
+
+search_button.addEventListener('click', () => {
+    input.classList.toggle("is-active")
+})
+
+
+function keyCode(event) {
+    var x = event.keyCode;
+    if (x == 27) {
+        input.classList.remove("is-active");
+    }
+}
+
+document.addEventListener("keydown", keyCode);
