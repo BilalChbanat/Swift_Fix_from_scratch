10a17fa241b884672a057e1c3c06f0633337675f
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-10-23 16:11:35 +0100

    form validation email and username and message

--- javaScript/script.js
+++ javaScript/script.js
@@ -5,6 +5,10 @@ const drop_down_menu_list = document.querySelector('.ul-drop-down-items');
 const search_button = document.querySelector('.search-button');
 const input = document.querySelector('.search-bar');
 const drop = document.getElementById('drop_down_menu');
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
 
 function hamburger() {
     hamburger_Button.classList.toggle('is-active');
@@ -29,3 +33,42 @@ function keyCode(event) {
 }
 
 document.addEventListener("keydown", keyCode);
+
+
+//============================ form validation ========================
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    validateInputs();
+})
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = username.value.trim();
+    const messageValue = message.value.trim();
+
+    if (usernameValue === '') {
+        username.style = "border: 1px solid red"
+    } else {
+        username.style = "border: 2px solid green"
+    }
+
+    if (emailValue === '') {
+        email.style = "border: 1px solid red"
+    } else if (!isValidEmail(emailValue)) {
+        email.style = "border: 1px solid green"
+    } else {
+        email.style = "border: 2px solid red"
+    }
+
+    if (messageValue === '') {
+        message.style = "border: 1px solid red"
+    } else {
+        message.style = "border: 2px solid green"
+    }
+
+};
+
